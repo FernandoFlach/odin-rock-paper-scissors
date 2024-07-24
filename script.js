92aa67dae9f883da8a201a7d6e51fa87723533aa
Fernando Flach <fernandoflach88@gmail.com>
2024-07-24 19:13:17 -0300

    Add prompt to get human choice and calculate winner

--- script.js
+++ script.js
@@ -1,19 +1,74 @@
 
 let option;
+let human_score = 0;
+let computer_score = 0;
 
 function getComputerChoice(){
     x = Math.random()
     if (x > 0.66){
-        option = "Rock"
+        option = "rock"
     }
     else if (x > 0.33){
-        option = "Scissors"
+        option = "scissors"
     }
     else{
-        option = "Paper"
+        option = "paper"
     }
+    return option
 
-    console.log(option)
 
 }
-getComputerChoice()
+
+function getHumanChoice(text = "Choose either rock, paper or scissors"){
+    x = prompt(text)
+    if (x == "rock" ||  x == "paper" || x == "scissors"){
+        return x
+    }
+    else {
+        getHumanChoice("Please insert a valid option")
+    }
+    
+}
+
+function winner(human, computer){
+    if(human == "paper"){
+        if(computer == "paper"){
+            return "draw"
+        }
+        else if(computer == "rock"){
+            return "human"
+        }
+        else {
+            return "computer"
+        }
+    }
+    else if(human == "rock"){
+
+        if(computer == "paper"){
+            return "computer"
+        }
+        else if(computer == "rock"){
+            return "draw"
+        }
+        else {
+            return "human"
+        }
+    }
+    else if(human == "scissors"){
+
+        if(computer == "paper"){
+            return "human"
+        }
+        else if(computer == "rock"){
+            return "computer"
+        }
+        else {
+            return "draw"
+        }
+}}
+
+let computer = getComputerChoice();
+let human = getHumanChoice();
+console.log("Computer:", computer)
+console.log("Human:", human)
+console.log("Winner is:", winner(human, computer));
